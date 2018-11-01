4555cc6e951687086e1da44a4ba237e693845168
MiniChii <f.p.chii@gmail.com>
2018-11-01 12:15:43 -0300

    Update src/main/resources/db/mysql/initDB.sql

--- src/main/resources/db/mysql/initDB.sql
+++ src/main/resources/db/mysql/initDB.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS petclinic;
+/*CREATE DATABASE IF NOT EXISTS petclinic;
 
 ALTER DATABASE petclinic
   DEFAULT CHARACTER SET utf8
@@ -80,3 +80,147 @@ CREATE TABLE IF NOT EXISTS roles (
   KEY fk_username_idx (username),
   CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username)
 ) engine=InnoDB;
+*/
+
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema gestion_inventario
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema gestion_inventario
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `gestion_inventario` DEFAULT CHARACTER SET utf8 ;
+USE `gestion_inventario` ;
+
+-- -----------------------------------------------------
+-- Table `gestion_inventario`.`Especie`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gestion_inventario`.`Especie` (
+  `id` INT NOT NULL,
+  `nombre` VARCHAR(45) NULL,
+  `etapa` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `gestion_inventario`.`Estante`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gestion_inventario`.`Estante` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `num_repisas` INT NULL,
+  `max_volumen` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `gestion_inventario`.`Cuadrante`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gestion_inventario`.`Cuadrante` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `max_estante` INT NULL,
+  `estante_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Cuadrante_Estante1_idx` (`estante_id` ASC) ,
+  CONSTRAINT `fk_Cuadrante_Estante1`
+    FOREIGN KEY (`estante_id`)
+    REFERENCES `gestion_inventario`.`Estante` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `gestion_inventario`.`Producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gestion_inventario`.`Producto` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `unidad_medida` INT NULL,
+  `precio` INT NULL,
+  `contenido` INT NULL,
+  `num_minimo` INT NULL,
+  `especie_id` INT NOT NULL,
+  `cuadrante_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Producto_Especie1_idx` (`especie_id` ASC) ,
+  INDEX `fk_Producto_Cuadrante1_idx` (`cuadrante_id` ASC) ,
+  CONSTRAINT `fk_Producto_Especie1`
+    FOREIGN KEY (`especie_id`)
+    REFERENCES `gestion_inventario`.`Especie` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Producto_Cuadrante1`
+    FOREIGN KEY (`cuadrante_id`)
+    REFERENCES `gestion_inventario`.`Cuadrante` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `gestion_inventario`.`Movimiento`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gestion_inventario`.`Movimiento` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cantidad_salida` INT NULL,
+  `fecha_salida` DATE NULL,
+  `producto_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Movimiento_Producto1_idx` (`producto_id` ASC) ,
+  CONSTRAINT `fk_Movimiento_Producto1`
+    FOREIGN KEY (`producto_id`)
+    REFERENCES `gestion_inventario`.`Producto` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `gestion_inventario`.`Proveedor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gestion_inventario`.`Proveedor` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `rut` VARCHAR(10) NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `telefono_contacto` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `gestion_inventario`.`Lote`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gestion_inventario`.`Lote` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `num_unidades` INT NOT NULL,
+  `fecha_ingreso` DATETIME NOT NULL,
+  `fecha_vencimiento` DATETIME NULL,
+  `proveedor_id` INT NOT NULL,
+  `producto_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Lote_Proveedor1_idx` (`proveedor_id` ASC) ,
+  INDEX `fk_Lote_Producto1_idx` (`producto_id` ASC) ,
+  CONSTRAINT `fk_Lote_Proveedor1`
+    FOREIGN KEY (`proveedor_id`)
+    REFERENCES `gestion_inventario`.`Proveedor` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Lote_Producto1`
+    FOREIGN KEY (`producto_id`)
+    REFERENCES `gestion_inventario`.`Producto` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
